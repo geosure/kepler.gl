9da70449fa84724f4507c0174a0de21068a54d33
Ricky Padilla <rickycpadilla@gmail.com>
2024-11-25 14:59:49 -0700

    Adjust kepler modal for city selection

--- examples/demo-app/src/_geosure/geosureSampleMaps.js
+++ examples/demo-app/src/_geosure/geosureSampleMaps.js
@@ -25,7 +25,7 @@ export const geosureSampleMaps = [
     // "detail": "The dataset contains a list of 2.5+ magnitude earthquakes in california. Information was generated using USGS website and contains multiple properties (location, magnitude, magtype) for each single entry. Data source: [USGS](https://earthquake.usgs.gov/data/data.php)",
     "size": 54936,
     "visible": true,
-    "dataUrl": "https://raw.githubusercontent.com/keplergl/kepler.gl-data/master/earthquakes/data.csv",
+    "dataUrl": "https://raw.githubusercontent.com/keplergl/kepler.gl-data/master/ukcommute/data.csv",
     // "configUrl": "https://raw.githubusercontent.com/keplergl/kepler.gl-data/master/earthquakes/config.json",
     "geosureConfig": londonConfig
   },
@@ -34,11 +34,11 @@ export const geosureSampleMaps = [
     "label": "New York",
     "queryType": "sample",
     "imageUrl": "https://images.unsplash.com/42/U7Fc1sy5SCUDIu4tlJY3_NY_by_PhilippHenzler_philmotion.de.jpg?q=40&h=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "description": "City New York, United States",
+    "description": "City in New York, United States",
     // "detail": "The dataset contains a list of 2.5+ magnitude earthquakes in california. Information was generated using USGS website and contains multiple properties (location, magnitude, magtype) for each single entry. Data source: [USGS](https://earthquake.usgs.gov/data/data.php)",
     "size": 54936,
     "visible": true,
-    "dataUrl": "https://raw.githubusercontent.com/keplergl/kepler.gl-data/master/earthquakes/data.csv",
+    "dataUrl": "https://raw.githubusercontent.com/keplergl/kepler.gl-data/master/nyc_earnings/data.arrow",
     // "configUrl": "https://raw.githubusercontent.com/keplergl/kepler.gl-data/master/earthquakes/config.json",
     "geosureConfig": newyorkConfig
   }
